467fafe7d6a512071913af943e48227c53a48dca
amrmynd <ariley.amy@gmail.com>
2019-11-11 16:18:42 +0100

    using BeforeEach

--- src/test/java/MathUtilsTest.java
+++ src/test/java/MathUtilsTest.java
@@ -1,12 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MathUtilsTest {
 
+    MathUtils mathUtils;
+
+    @BeforeEach
+    void init() {
+        mathUtils = new MathUtils();
+    }
+
     @Test
     void testAdd() {
-        MathUtils mathUtils = new MathUtils();
         int expected = 2;
         int actual = mathUtils.add(1, 1);
         assertEquals(expected, actual, "The add method should add two numbers");
@@ -14,13 +21,11 @@ class MathUtilsTest {
 
     @Test
     void testDivide() {
-        final MathUtils mathUtils = new MathUtils();
         assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "Divide by 0 should throw");
     }
 
     @Test
     void testComputeCircleRadius() {
-        MathUtils mathUtils = new MathUtils();
         assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return correct circle area");
     }
 }
