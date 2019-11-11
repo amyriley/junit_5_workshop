58794919dff4a71bf323eacb642260e8a66f31c6
amrmynd <ariley.amy@gmail.com>
2019-11-11 14:41:21 +0100

    adding a test class

---
+++ src/test/java/MathUtilsTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtilsTest {
+
+    @Test
+    void test() {
+        System.out.println("This test ran");
+    }
+
+}
