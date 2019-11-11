953b273c46f0ad034cc6f2562a1303cd4560e171
amrmynd <ariley.amy@gmail.com>
2019-11-11 16:45:41 +0100

    using assertAll

--- src/test/java/MathUtilsTest.java
+++ src/test/java/MathUtilsTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,16 +18,31 @@ class MathUtilsTest {
     void testAdd() {
         int expected = 2;
         int actual = mathUtils.add(1, 1);
-        assertEquals(expected, actual, "The add method should add two numbers");
+        assertEquals(expected, actual,
+                "Add method should return the sum of two numbers");
     }
 
     @Test
+    @DisplayName("Testing multiply method")
+    void testMultiply() {
+        assertAll(
+                () -> assertEquals(4, mathUtils.multiply(2,2)),
+                () -> assertEquals(0, mathUtils.multiply(2,0)),
+                () -> assertEquals(-2, mathUtils.multiply(2,-1))
+        );
+    }
+
+    @Test
+    @DisplayName("Testing divide method")
     void testDivide() {
-        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "Divide by 0 should throw");
+        assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0),
+                "Divide by 0 should throw");
     }
 
     @Test
+    @DisplayName("Testing compute circle radius method")
     void testComputeCircleRadius() {
-        assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return correct circle area");
+        assertEquals(314.1592653589793, mathUtils.computeCircleArea(10),
+                "Should return correct circle area");
     }
 }
