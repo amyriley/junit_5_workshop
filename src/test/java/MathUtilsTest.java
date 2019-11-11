168fc9a1d365a6a1e872d56b0208456f0332291a
amrmynd <ariley.amy@gmail.com>
2019-11-11 14:59:44 +0100

    testing the add method

--- src/test/java/MathUtilsTest.java
+++ src/test/java/MathUtilsTest.java
@@ -6,7 +6,10 @@ class MathUtilsTest {
 
     @Test
     void test() {
-        System.out.println("This test ran");
+        MathUtils mathUtils = new MathUtils();
+        int expected = 2;
+        int actual = mathUtils.add(1, 1);
+        assertEquals(expected, actual);
     }
 
 }
